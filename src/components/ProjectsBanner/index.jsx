34e33ec61aa55b9a86d8484c10b93f8f0116bee9
Luiz Lopes <luizlopes12@outlook.com>
2022-04-05 17:02:34 -0300

    Adicionando Projetos

--- src/components/ProjectsBanner/index.jsx
+++ src/components/ProjectsBanner/index.jsx
@@ -6,6 +6,14 @@ import enver from '../../img/Enver.png'
 import todo from '../../img/todo.png'
 import leafly from '../../img/leafly.png'
 import fruitz from '../../img/fruitz.png'
+import letmeask from '../../img/letmeask.png'
+import cep from '../../img/cep.png'
+import cep2 from '../../img/cep2.png'
+import trii from '../../img/trii.png'
+import loop from '../../img/loop.png'
+
+
+
 import ProjectsItem from '../ProjectsItem'
 const ProjectsBanner = () => {
   return (
@@ -20,6 +28,16 @@ const ProjectsBanner = () => {
       tech='React, Redux, Styled Components e Firebase'
       linkRepo='https://github.com/luizlopes12/BeTheHero'
       linkSite='https://bethehero24.web.app/'
+      />
+      <ProjectsItem 
+      img={letmeask}
+      title='Let Me Ask'
+      desc='Sistema onde usuarios podem entrar em uma sala e enviar perguntas, dar like, deixar em destaque e excluir perguntas,
+      esse tipo de sistema pode ser útil em uma live, onde as pessoas deixam ali as peguntas e o streamer consegue marcar as que 
+      ele já respondeu através do painel de administrador da aplicação.'
+      tech='React, Context API, Styled Components e Firebase'
+      linkRepo='https://github.com/luizlopes12/LetMeAsk'
+      linkSite='https://letmeask-14c72.web.app/'
       />
       <ProjectsItem 
       img={feedback}
@@ -38,6 +56,14 @@ const ProjectsBanner = () => {
       linkRepo='https://github.com/luizlopes12/Enver'
       linkSite='https://luizlopes12.github.io/Enver/'
       />
+      <ProjectsItem 
+      img={loop}
+      title='Loopstudios'
+      desc='Landing Page para empresa de desenvolvimento e realidade virtual.'
+      tech='HTML, CSS, Javascript e Bootstrap'
+      linkRepo='https://github.com/luizlopes12/Loopstudios'
+      linkSite='https://luizlopes12.github.io/Loopstudios/'
+      />
       <h1>Funcionalidades</h1>
       <ProjectsItem 
       img={todo}
@@ -55,6 +81,14 @@ const ProjectsBanner = () => {
       linkRepo='https://github.com/luizlopes12/Leafly'
       linkSite='https://luizlopes12.github.io/Leafly/'
       />
+      <ProjectsItem 
+      img={cep}
+      title='CEP Search'
+      desc='Funcionalidade que busca a cidade e estado através do CEP, utilizando a API viaCEP.'
+      tech='React, Axios e Material UI'
+      linkRepo='https://github.com/luizlopes12/CEP_search'
+      linkSite='https://luizlopes12.github.io/CEP_search/'
+      />
       <h1>Desafios de empresas</h1>
       <ProjectsItem 
       img={fruitz}
@@ -63,6 +97,22 @@ const ProjectsBanner = () => {
       tech='React, Axios, Bootstrap e React-Bootstrap'
       linkRepo='https://github.com/luizlopes12/Fruitz'
       linkSite='https://fruitz.vercel.app/'
+      />
+      <ProjectsItem 
+      img={cep2}
+      title='CEP Info'
+      desc='Desafio para estagiário front end na DivTech, o objetivo era consumir a api ViaCEP para verificar a cidade e estado do usuario.'
+      tech='HTML, CSS, Javascript e Jquery'
+      linkRepo='https://github.com/luizlopes12/Fruitz'
+      linkSite='https://fruitz.vercel.app/'
+      />
+      <ProjectsItem 
+      img={trii}
+      title='Trii Forms'
+      desc='Desafio para estagiário front end na Trii, contruir um formulario interativo com confirmação, mensagem de erro e alertas.'
+      tech='HTML, CSS e Javascript'
+      linkRepo='https://github.com/luizlopes12/Trii_forms'
+      linkSite='https://luizlopes12.github.io/Trii_forms/'
       />
     </ProjectsBannerStyle>
   )
