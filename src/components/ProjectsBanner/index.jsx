57789b7211acf7521f563b2a9395569477fa065f
Luiz Lopes <luizlopes12@outlook.com>
2022-04-05 14:58:41 -0300

    Adicionando projetos

--- src/components/ProjectsBanner/index.jsx
+++ src/components/ProjectsBanner/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { ProjectsBannerStyle } from './styled'
 import hero from '../../img/Bethehero.png'
+import feedback from '../../img/feedback.png'
 import enver from '../../img/Enver.png'
 import todo from '../../img/todo.png'
+import leafly from '../../img/leafly.png'
+import fruitz from '../../img/fruitz.png'
 import ProjectsItem from '../ProjectsItem'
 const ProjectsBanner = () => {
   return (
@@ -18,12 +21,20 @@ const ProjectsBanner = () => {
       linkRepo='https://github.com/luizlopes12/BeTheHero'
       linkSite='https://bethehero24.web.app/'
       />
+      <ProjectsItem 
+      img={feedback}
+      title='Feedback UI'
+      desc='Front End de uma pagina de captura de feedbacks sobre algum produto ou serviço.'
+      tech='React e CSS'
+      linkRepo='https://github.com/luizlopes12/Enver'
+      linkSite='https://luizlopes12.github.io/Enver/'
+      />
       <h1>Landing Pages</h1>
       <ProjectsItem 
       img={enver}
       title='Enver'
       desc='Landing Page para empresa de desenvolvimento e design digital.'
-      tech='React, Styled Components'
+      tech='React e Styled Components'
       linkRepo='https://github.com/luizlopes12/Enver'
       linkSite='https://luizlopes12.github.io/Enver/'
       />
@@ -32,18 +43,26 @@ const ProjectsBanner = () => {
       img={todo}
       title='Todo List'
       desc='Todo list basica, salvando os dados localmente.'
-      tech='React e material UI'
+      tech='React, Styled Components e material UI'
       linkRepo='https://github.com/luizlopes12/Todo_list'
       linkSite='https://luizlopes12.github.io/Todo_list/'
       />
+      <ProjectsItem 
+      img={leafly}
+      title='Leafly'
+      desc='App de clima básico, utilizando a localização atual do usuario, utilizando os dados da API OpenWeatherMap.'
+      tech='React, Axios e Bootstrap'
+      linkRepo='https://github.com/luizlopes12/Leafly'
+      linkSite='https://luizlopes12.github.io/Leafly/'
+      />
       <h1>Desafios de empresas</h1>
       <ProjectsItem 
-      img={enver}
-      title='Enver'
-      desc='Landing Page para empresa de desenvolvimento e design digital.'
-      tech='React, Styled Components'
-      linkRepo='https://github.com/luizlopes12/Enver'
-      linkSite='https://luizlopes12.github.io/Enver/'
+      img={fruitz}
+      title='Fruitz'
+      desc='Desafio para estagiário front end na Frexco, o objetivo era construir um sistema de carrinho de compras consumindo a API Fruitvice (necessario a extensão "Moesif Origin & CORS Changer" do google chrome para funcionar pois a API não possui CORS).'
+      tech='React, Axios, Bootstrap e React-Bootstrap'
+      linkRepo='https://github.com/luizlopes12/Fruitz'
+      linkSite='https://fruitz.vercel.app/'
       />
     </ProjectsBannerStyle>
   )
